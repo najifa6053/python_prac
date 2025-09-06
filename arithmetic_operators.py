2ca0e14ebaa9c65971f72411c4c52c2238682527
najifa6053 <esha15-6053@diu.edu.bd>
2025-09-06 12:32:00 +0600

    first commit

---
+++ arithmetic_operators.py
@@ -0,0 +1,15 @@
+a = 10
+b = 5
+
+print("Arithmetic Operators in Python")
+print("-----------------------------")
+print("a =", a)
+print("b =", b)
+
+print("Addition (a + b):", a + b)          # Addition
+print("Subtraction (a - b):", a - b)       # Subtraction
+print("Multiplication (a * b):", a * b)    # Multiplication
+print("Division (a / b):", a / b)          # Division
+print("Floor Division (a // b):", a // b)   # Floor Division
+print("Modulus (a % b):", a % b)           # Modulus
+print("Exponentiation (a ** b):", a ** b)   # Exponentiation
