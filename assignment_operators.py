6c5eb4b6727c3d4b54d2bd942fcc35a9e09f8e19
najifa6053 <esha15-6053@diu.edu.bd>
2025-09-06 12:55:29 +0600

    Assignment operators

---
+++ assignment_operators.py
@@ -0,0 +1,16 @@
+num = 10
+
+print("Assignment Operators in Python")
+print("-----------------------------")
+
+print("Initial value of num:", num)
+num += 5
+print("After += 5:", num)
+num -= 3
+print("After -= 3:", num)
+num *= 2
+print("After *= 2:", num)
+num /= 4
+print("After /= 4:", num)
+num %= 3
+print("After %= 3:", num)
