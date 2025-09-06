21f700fbbfca42f601e181685ab5818bda9fe747
najifa6053 <esha15-6053@diu.edu.bd>
2025-09-06 14:58:26 +0600

    if else elif

---
+++ if_elif_else.py
@@ -0,0 +1,16 @@
+num = 10
+
+if num > 50:
+    if num > 90:
+        print("A+")
+    elif num > 80:
+        print("A")
+    elif num > 70:
+        print("B")
+    elif num > 60:
+        print("C")
+    else:
+        print("D")
+
+else:
+    print("Fail")
